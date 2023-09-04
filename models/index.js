cc0e2d0505033dbb3a6efb6929ba838f622237a0
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-09-04 11:29:45 +0200

    Delete models directory

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const Users = require("./users");
-const Products = require("./products");
-const Orders = require("./orders");
-
-module.exports = {
-  users: new Users(),
-  products: new Products(),
-  orders: new Orders(),
-};
