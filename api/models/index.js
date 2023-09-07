b6ba092da71b5f404f99e1f87595dc934474375e
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-09-07 10:00:55 +0200

    Delete api directory

--- api/models/index.js
+++
@@ -1,9 +0,0 @@
-const Users = require("./users");
-const Products = require("./products"); 
-const Cart = require("./cart");
-
-module.exports = {
-    users: new Users(), 
-    products: new Products(),
-    cart: new Cart()
-}
