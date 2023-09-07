34297e0ee6ba1bb245284de6c3380a3206c0c5bc
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-09-07 09:58:37 +0200

    Add files via upload

---
+++ api/middleware/authenticateUser.js
@@ -0,0 +1,50 @@
+const { sign, verify } = require("jsonwebtoken");
+require("dotenv").config();
+
+function createToken(user) {
+  return sign(
+    {
+      emailAdd: user.emailAdd,
+      userPass: user.userPass,
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "1h",
+    }
+  );
+}
+
+function verifyAToken(req, res, next) {
+  try {
+    const token =
+      req.cookies && req.cookies["AuthorisedUser"]
+        ? req.cookies["AuthorisedUser"]
+        : null;
+
+    if (!token) {
+      return res.json({
+        status: res.statusCode,
+        message: "Please register",
+      });
+    }
+
+    const isValid = verify(token, process.env.SECRET_KEY);
+
+    if (isValid) {
+      req.authenticated = true;
+      return next();
+    } else {
+      return res.json({
+        status: res.statusCode,
+        message: "Token is not valid",
+      });
+    }
+  } catch (err) {
+    return res.json({
+      status: res.statusCode,
+      message: err.message,
+    });
+  }
+}
+
+module.exports = { createToken, verifyAToken };
