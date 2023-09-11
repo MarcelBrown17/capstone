a4b75154007cd1f9c658a7420aa92f5f3e8468e8
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-09-11 15:02:34 +0200

    new api gain

--- api/models/users.js
+++ api/models/users.js
@@ -43,94 +43,96 @@ class Users {
     });
   }
 
- 
-  async register(req, res) {
-    const data = req.body;
-  
-    try {
-      const saltRounds = 15;
-      const hashedPassword = await bcrypt.hash(data.UserPass, saltRounds);
-  
-      const user = {
-        firstName: data.firstName,
-        lastName: data.lastName,
-        userRole: data.userRole,
-        emailAdd: data.emailAdd,
-        userPass: hashedPassword
-      };
-  
-      const query = 'INSERT INTO Users SET ?';
-      db.query(query, user, (err) => {
-        if (err) {
-          console.error('Error inserting user:', err);
-          res.status(500).json({ error: 'Internal server error' });
-        } else {
-          const token = jwt.sign(
-            { userID: results[0].userID, emailAdd: results[0].emailAdd },
-            process.env.secret_key,
-            {
-              expiresIn: '1h',
-            }
-          );
-          
-  
-          res.cookie('AuthorizedUser', token, {
-            maxAge: 3600000,
-            httpOnly: true,
+async register(req, res) {
+  const data = req.body;
+
+  try {
+    const saltRounds = 15;
+    const hashedPassword = await bcrypt.hash(data.userPass, saltRounds);
+
+    const user = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      userRole: data.userRole,
+      emailAdd: data.emailAdd,
+      userPass: hashedPassword
+    };
+
+    const query = 'INSERT INTO Users SET ?';
+    db.query(query, user, (err, results) => {
+      if (err) {
+        console.error('Error inserting user:', err);
+        res.status(500).json({ error: 'Internal server error' });
+      } else {
+        const token = jwt.sign(
+          { userID: results.insertId, emailAdd: user.emailAdd },
+          process.env.secret_key,
+          {
+            expiresIn: '1h',
+          }
+        );
+
+        res.cookie('AuthorizedUser', token, {
+          maxAge: 3600000,
+          httpOnly: true,
+        });
+
+        res.status(201).json({
+          status: res.statusCode,
+          message: 'New user registered!',
+        });
+      }
+    });
+  } catch (error) {
+    console.error('Error hashing password:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+async login(req, res) {
+  const { emailAdd, userPass } = req.body;
+  const query = `
+    SELECT userID, firstName, lastName, userRole, emailAdd, userPass
+    FROM Users
+    WHERE emailAdd = '${emailAdd}';
+  `;
+
+  db.query(query, async (err, results) => {
+    if (err) throw err;
+
+    if (!results?.length) {
+      res.json({
+        status: res.statusCode,
+        msg: "You provided a wrong email.",
+      });
+    } else {
+      const user = results[0];
+
+      await compare(userPass, user.userPass, (cErr, cResults) => {
+        if (cErr) throw cErr;
+
+        if (cResults) {
+          const token = createToken({
+            emailAdd,
+            userPass,
           });
-  
-          res.status(201).json({
+
+          res.json({
+            msg: "Welcome to Envy Essentials",
+            token,
+            results: user,
+          });
+        } else {
+          res.json({
             status: res.statusCode,
-            message: 'New user registered!',
+            msg: "Invalid password or you have not registered",
           });
         }
       });
-    } catch (error) {
-      console.error('Error hashing password:', error);
-      res.status(500).json({ error: 'Internal server error' });
     }
-  }
-  
-   async login(req,res){
-    const {emailAdd, userPass} =
-    req.body
-    const query =
-    `
-    SELECT firstName , lastName, userRole , emailAdd , userPass FROM Users
-    WHERE emailAdd = '${emailAdd}';
-    `
-    db.query(query,async(err, results)=>{
-        if(err) throw err
-        if(!results?.length){
-            res.json({
-                status:res.statusCode,
-                msg : "You provided a wrong email."
-            })
-        }else{
-            await compare(userPass, results[0].userPass,(cErr,cResults)=>{
-                if(cErr) throw cErr
-                // Create a token
-                const token = createToken({
-                    emailAdd, userPass
-                })
-                if(cResults){
-                    res.json({
-                        msg: "Welcome to Envy Essentials",
-                        token,
-                        results: results[0]
-                    })
-                }else{
-                    res.json({
-                        status: res.statusCode,
-                        msg: "Invalid password or you have not registered"
-                    })
-                }
-            })
-        }
-    })
+  });
 }
-  
-  
+
   async updateUser(req, res) {
     const query = `
         UPDATE Users 
