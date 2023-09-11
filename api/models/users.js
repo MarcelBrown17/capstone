35fa8eac1094559be246241445166303a205f86b
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-09-11 10:04:38 +0200

    user update

--- api/models/users.js
+++ api/models/users.js
@@ -1,6 +1,13 @@
 const db = require("../config");
 const { hash, compare } = require("bcrypt");
 const { createToken } = require("../middleware/authenticateUser");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const app = express();
+app.use(cookieParser());
 
 class Users {
   fetchUsers(req, res) {
@@ -46,7 +53,7 @@ class Users {
 
     try {
       // password encryption
-      const saltRounds = 10; //number of salt rounds
+      const saltRounds = 10; // number of salt rounds
       const hashedPassword = await hash(data.userPass, saltRounds);
 
       // Replace the plain password with the hashed password
@@ -70,102 +77,100 @@ class Users {
             status: 500,
             message: "Internal Server Error",
           });
-        }
+        } else {
+          const token = jwt.sign(
+            { userID: data.userID, email: data.emailAdd },
+            process.env.SECRET_KEY, // Make sure you have this in your .env file
+            {
+              expiresIn: "1h",
+            }
+          );
 
-        // create token
-        let token = createToken(user);
+          res.cookie("AuthorizedUser", token, {
+            maxAge: 3600000,
+            httpOnly: true,
+          });
 
-        res.json({
-          status: res.statusCode,
-          message: "New user registered",
-        });
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        status: 500,
-        message: "Internal Server Error",
-      });
-    }
-  }
-
-  login(req, res) {
-    const { emailAdd, userPass } = req.body;
-
-    if (!emailAdd || !userPass) {
-      return res.status(400).json({
-        status: 400,
-        message: "Email and password are required",
-      });
-    }
-
-    // query
-    const query = `
-        SELECT firstName, lastName, emailAdd, userPass 
-        FROM Users 
-        WHERE emailAdd = ?;`;
-
-    db.query(query, [emailAdd], async (err, result) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({
-          status: 500,
-          message: "Internal Server Error",
-        });
-      }
-
-      if (!result?.length) {
-        return res.status(401).json({
-          status: 401,
-          message: "Incorrect Email Address",
-        });
-      } else {
-        const storedPasswordHash = result[0].userPass;
-
-        try {
-          const passwordMatch = await compare(userPass, storedPasswordHash);
-
-          if (passwordMatch) {
-            // create token
-            const user = {
-              emailAdd,
-              userPass: storedPasswordHash,
-            };
-            const token = createToken(user);
-
-            res.json({
-              status: res.statusCode,
-              message: "Welcome to Envy Essentials",
-              token,
-              result: result[0],
-            });
-          } else {
-            res.status(401).json({
-              status: 401,
-              message: "Unauthorized user or incorrect password",
-            });
-          }
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({
-            status: 500,
-            message: "Internal Server Error",
+          res.status(201).json({
+            status: res.statusCode,
+            message: "New user registered!",
           });
         }
-      }
-    });
+      });
+    } catch (error) {
+      console.error("Error hashing password:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
 
+  async login(req, res) {
+    const { email, userPass } = req.body;
+  
+    try {
+      const query = `
+        SELECT userID, firstName, lastName, emailAdd, userPass
+        FROM Users
+        WHERE emailAdd = ?;
+      `;
+  
+      db.query(query, [email], async (err, result) => {
+        if (err) {
+          console.error('Error querying the database:', err);
+          return res.status(500).json({ error: 'Internal server error' });
+        }
+  
+        if (!result?.length) {
+          return res.status(401).json({
+            status: res.statusCode,
+            message: 'Incorrect email or password!',
+          });
+        }
+  
+        const user = result[0];
+        const isPasswordValid = await compare(userPass, user.userPass); // Use compare from bcrypt
+  
+        if (isPasswordValid) {
+          const token = jwt.sign(
+            { userID: user.userID, email: user.emailAdd },
+            process.env.secret_key, 
+            {
+              expiresIn: '1h',
+            }
+          );
+  
+          res.cookie('AuthorizedUser', token, {
+            maxAge: 3600000,
+            httpOnly: true,
+          });
+  
+          return res.status(200).json({
+            message: 'Welcome back to Envy Essentials!',
+            token,
+            user,
+          });
+        } else {
+          return res.status(401).json({
+            status: res.statusCode,
+            message: 'Incorrect email or password!',
+          });
+        }
+      });
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+  
   updateUser(req, res) {
     const query = `
         UPDATE Users 
-        SET? 
+        SET ? 
         WHERE userID = ?`;
 
     const data = req.body;
 
     // encrypt password
-    data.userPass = hashSync(data.userPass, 20);
+    data.userPass = hash(data.userPass, 20); // Changed from hashSync
 
     db.query(query, [data, req.params.id], (err) => {
       if (err) throw err;
